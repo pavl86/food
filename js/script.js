0bd96ba9139f2ba93afa66d74f520ac66b1e6f73
Pavel <p.6a@tut.by>
2021-12-29 14:45:41 +0300

    refactor: add babel compiler

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import cards from './modules/cards';
 import calc from './modules/calc';
 import forms from './modules/forms';
